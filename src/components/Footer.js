66d989bcbc6b5f053f080c90ecce6f65575a7443
Maxis Kao <maxiskao@yahoo-inc.com>
2016-09-03 17:00:01 +0800

    rewrap footer element

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,21 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div id="footer">
-        URL Monster 1.0 &copy;&nbsp;&nbsp;<a href="http://gadget.chienwen.net/">Handy Gadgets</a>
-        &nbsp;|&nbsp;
-        <a href="https://nodejs.org/api/url.html">Node.js</a> 
-        &nbsp;|&nbsp;
-        <a href="https://facebook.github.io/react/">React</a>
-      </div>
+      <footer id="footer">
+        <span>
+          <a href="https://github.com/Purple-Ninja/url-monster">URL Monster 1.0</a>
+        </span>
+        <span> &copy; </span>
+        <span className="sep">
+          <a href="http://gadget.chienwen.net/">Handy Gadgets</a>
+        </span>
+        <span className="sep">
+          <a href="https://nodejs.org/api/url.html">Node.js</a>
+        </span>
+        <span className="sep">
+          <a href="https://facebook.github.io/react/">React</a>
+        </span>
+      </footer>
     );
   }
 }
