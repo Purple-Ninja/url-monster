52cde9077f3479382190c367050a5eaaae8422a7
Jason Huang <cjhuang@yahoo-inc.com>
2016-09-06 23:50:30 +0800

    add unit test for #getDiffFields

--- src/App.test.js
+++ src/App.test.js
@@ -30,4 +30,34 @@ describe('<App />', () => {
     const app = shallow(<App {...appProps} />);
     expect(app.find(Messages).length).toBe(1);
   });
+
+  describe('#getDiffFields', () => {
+    let getDiffFields;
+
+    beforeEach(() => {
+      const appInstance = shallow(<App />).instance();
+      getDiffFields = appInstance.getDiffFields.bind(appInstance);
+    });
+
+    it('should show extra properties', () => {
+      const foo = { a: 1, b: 2 };
+      const bar = { b: 2, c: 3 };
+
+      expect(getDiffFields(foo, bar)).toEqual(['a']);
+    });
+
+    it('should detect value difference', () => {
+      const foo = { a: 1, b: 2 };
+      const bar = { b: 200, c: 3 };
+
+      expect(getDiffFields(foo, bar)).toEqual(['a', 'b']);
+    });
+
+    it('should detect extra properties from both sides', () => {
+      const foo = { a: 1, b: 2 };
+      const bar = { b: 200, c: 3 };
+
+      expect(getDiffFields(foo, bar, true)).toEqual(['a', 'b', 'c']);
+    });
+  });
 });
