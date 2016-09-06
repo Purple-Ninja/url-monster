efcc24a69dc5f5506879507c9507e4b32d697210
Jason Huang <cjhuang@yahoo-inc.com>
2016-09-07 00:02:58 +0800

    add unit test for #compareUrls

--- src/App.test.js
+++ src/App.test.js
@@ -31,6 +31,39 @@ describe('<App />', () => {
     expect(app.find(Messages).length).toBe(1);
   });
 
+  describe('#compareUrls', () => {
+    let compareUrls;
+
+    beforeEach(() => {
+      const appInstance = shallow(<App />).instance();
+      compareUrls = appInstance.compareUrls.bind(appInstance);
+    });
+
+    it('should parse and tell the difference', () => {
+      const foo = 'https://tw.yahoo.com/?bucket=181&query=string';
+      const bar = 'http://user:pass@host.com:8080/p/a/t/h?query=string#hash';
+
+      expect(compareUrls([foo, bar])).toEqual({
+        isSame: false,
+        diffFields: ['protocol', 'auth', 'hostname', 'port', 'pathname', 'query', 'hash'],
+        queryAllFields: ['bucket', 'query'],
+        queryDiffFields: ['bucket']
+      });
+    });
+
+    it('should ignore the order of queries', () => {
+      const foo = 'https://tw.yahoo.com/?a=a&b=b';
+      const bar = 'https://tw.yahoo.com/?b=b&a=a';
+
+      expect(compareUrls([foo, bar])).toEqual({
+        isSame: true,
+        diffFields: [],
+        queryAllFields: ['a', 'b'],
+        queryDiffFields: []
+      });
+    });
+  });
+
   describe('#getDiffFields', () => {
     let getDiffFields;
 
