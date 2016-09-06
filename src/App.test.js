52d6f1ebde3472d4855cad803e3d1c3faaf35a2b
Jason Huang <cjhuang@yahoo-inc.com>
2016-09-06 15:57:15 +0800

    add unit test for <App />

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from './App';
+import UrlBox from './components/UrlBox';
+import Messages from './components/Messages';
+import Footer from './components/Footer';
+
+describe('<App />', () => {
+  it('renders with correct shape', () => {
+    const app = shallow(<App />);
+    expect(app.find(UrlBox).length).toEqual(2);
+    expect(app.find(Messages).length).toEqual(0);
+    expect(app.find(Footer).length).toEqual(1);
+  });
+
+  describe('<Messages />', () => {
+    it('renders no Message if url2 is empty', () => {
+      const appProps = {
+        urls: ['https://tw.yahoo.com/', '']
+      };
+      const app = shallow(<App {...appProps} />);
+      expect(app.find(Messages).length).toEqual(0);
+    });
+
+    it('renders Message if url2 is not empty', () => {
+      const appProps = {
+        urls: ['https://tw.yahoo.com/', 'https://tw.yahoo.com/']
+      };
+      const app = shallow(<App {...appProps} />);
+      expect(app.find(Messages).length).toEqual(1);
+    });
+  });
 });
